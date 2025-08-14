54a527691bf7d5e66060546a0fd66bde8c14c71d
AliceChatelain <alice.chatelain@davidson.fr>
2025-08-14 15:06:24 +0200

    ok

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -4,6 +4,15 @@ import {test} from '@playwright/test'
 
 test('the first test', async({page})=> {
     await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+
+})
+
+test('the first test', async({page})=> {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('DatePicker').click()
 
 })
 
